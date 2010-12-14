4404284f4535d292b904353a8b844a75e5e822c6
Swizec Teller <swizec@swizec.com>
2010-12-14 11:37:46 +0100

    beginnings of proper feed display

--- Next-Share/JSI/WebUI/Browse/views.py
+++ Next-Share/JSI/WebUI/Browse/views.py
@@ -1,6 +1,15 @@
 # Create your views here.
 
 from django.http import HttpResponse
+from django.conf import settings
+
+import os
+
+from lib import feedparser
 
 def begin(request):
-    return HttpResponse("Meow")
+    out = []
+    for feed in os.listdir(settings.FEED_DIR):
+        out.append(feedparser.parse(feed))
+    
+    return HttpResponse(out)
