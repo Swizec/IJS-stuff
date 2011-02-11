d9ff387bf1d65dcee460cf806e6d34b28c034b3c
Swizec Teller <swizec@swizec.com>
2011-02-11 16:19:11 +0100

    properly testing listing the root

--- Next-Share/JSI/ProviderToolbox-UI/Browse/tests.py
+++ Next-Share/JSI/ProviderToolbox-UI/Browse/tests.py
@@ -11,12 +11,23 @@ class BasicTest(TestCase):
         self.assertEquals(response.status_code, 200)
         self.assert_("<body>" in response.content)
 
+    def test_list_root(self):
+        response = self.client.get('/list_dir/', {'dir': '/'})
+
+        self.assertEquals(response.status_code, 200)
+
+        for feed in os.listdir(settings.FEED_DIR):
+            if ".properties" in os.listdir(settings.FEED_DIR+feed):
+                self.assert_(response.content.find(feed) >= 0)
+
     def test_list(self):
         feed = os.listdir(settings.FEED_DIR)[0]
 
-        html = self.client.get('/list_dir/',
-                               {'dir': feed}).content
+        response = self.client.get('/list_dir/',
+                                   {'dir': feed})
+
+        self.assertEquals(response.status_code, 200)
 
         for f in os.listdir(settings.FEED_DIR+feed):
             if f.endswith('.xml'):
-                self.assert_(html.find(f) >= 0)
+                self.assert_(response.content.find(f) >= 0)
